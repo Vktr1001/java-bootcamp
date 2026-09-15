2e92175c3aab87666cd46aa93864368c8b9e5b4b
Cyberdog1001 <vktr.1001@proton.me>
2026-09-15 13:15:40 -0600

    Rename project folders, replace team-exercise-1.py with count.py

---
+++ intro_scripts/count.py
@@ -0,0 +1,7 @@
+for number in range(1, 11):
+    if number < 5:
+        print(number, "- small number")
+    elif number == 5:
+        print(number, "- We reached the middle")
+    else:
+        print(number, "- large number")
