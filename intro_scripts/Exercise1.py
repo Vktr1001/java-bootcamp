d24d1d39be5227b7bef0357a80082d43ea4dba69
Cyberdog1001 <vktr.1001@proton.me>
2026-09-15 12:14:46 -0600

    BizzFuzz lab

---
+++ intro_scripts/Exercise1.py
@@ -0,0 +1,13 @@
+#Greeting = "Hello " + name + " welcome to " + ch
+#print(Greeting)
+
+#Exercise 1 Input/save data to a variable and print it out
+
+
+name = input("What is your name? : ")
+age = input("What is your age? : ")
+ch = input("What is your coho? : ")
+color = input("What is your favorite color? : ")
+music = input("What is your favorite music? : ")
+
+print("Hello " + name + " welcome to " + ch + " your age is " + age + " your favorite color is " + color + " and your favorite music is " + music)
---
+++ intro_scripts/hello.js
@@ -0,0 +1,6 @@
+console.log("I am Vktr1001");
+console.log("I'm learnoing JavaScript");
+console.log("Want to build a career in software engineering");
+
+const Username = "Cyberdog1001";
+console.log("My username is: " + Username);
