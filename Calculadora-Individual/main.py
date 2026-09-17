4426af48ce04c0a90e5d85e4d357622f22695458
Cyberdog1001 <vktr.1001@proton.me>
2026-09-17 10:41:37 -0600

    addition calculator

---
+++ Calculadora-Individual/main.py
@@ -0,0 +1,5 @@
+# Program that takes two input numbers, then adds them together and prints the result
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = num1 + num2
+print("The result is:", result) 
