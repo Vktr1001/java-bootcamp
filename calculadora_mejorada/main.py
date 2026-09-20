5cf0fdddefe193a4f5813450b3fdf8c1342c30e0
Cyberdog1001 <vktr.1001@proton.me>
2026-09-20 17:26:58 -0600

    directories name fix

---
+++ calculadora_mejorada/main.py
@@ -0,0 +1,17 @@
+def addmultiplenumbers(numbers) :
+    pass
+
+def multiplymultiplenumbers(numbers) :
+    pass
+
+def iseven(num) :
+    pass
+
+def isitainteger(num) :
+    pass
+
+def main() :
+    pass
+
+if __name__ == "__main__" :
+    main()
